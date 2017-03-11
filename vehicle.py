95a50a8e6d72cc0b8e3cc4e99570fdb21602d9d7
Sebastiaan Pasterkamp <26205277+SebastiaanPasterkamp@users.noreply.github.com>
2017-03-11 23:12:23 +0100

    Car: Implemented steering

--- vehicle.py
+++ vehicle.py
@@ -2,6 +2,10 @@
 """Vehicle set to provide control and motion models for various types of
 vehicles"""
 
+import os
+import sys
+import json
+
 import RPi.GPIO as GPIO
 from sunfounder.PCA9685 import PWM
 import time    # Import necessary modules
@@ -22,47 +26,74 @@ class Car:
         5   # servo driver IC CH5
         ]
 
-    # Pins to set HIGH when moving forward
+    # Motor GPIO pins to set HIGH when moving forward
     forward = [
         11, # pin11 - motor 0
-        12  # pin12 - motor 1
+        13  # pin13 - motor 1
         ]
-    # Pins to set HIGH when moving backwards
+    # Motor GPIO pins to set HIGH when moving backwards
     backwards = [
-        13, # pin13 - motor 0
+        12, # pin12 - motor 0
         15  # pin15 - motor 1
         ]
     speed = 0.0
-    #target_speed = 0.0
-    #acceleration = 20.0
-    #max_speed = 0.0
+    speed_max = 4000.0 # Max value for PWM
 
-    def __init__(pwm, config={}):
+    # Steering PWM pin
+    steering_controller = [
+        0 # servo driver IC CH0
+        ]
+    rotation = 0.0
+    rotation_max = 75.0
+    rotation_offset = 450.0 # Default servo position for 'home' or straight
+
+
+    def __init__(self, pwm, config={}, debug=False):
         """Create car type motion controller
 
         pwm = Sunfounder's PWM servo controller instance
 
         config options:
-        - flip_direction : list of motors who's direction is flipped
-
-        - min_rotation : minimum rotation - defines maximum left arc
-        - max_rotation : maximum rotation - defines maximum right arc
-                         the average defines "straight forward"
+        - flip_direction :  list of motors who's direction is flipped
+        - speed_max:        maximum speed for motor PWM controllers
+        - rotation_max :    maximum PWM for steering servo; maximum steering arc
+        - rotation_offset : change in default PWM for steering servo
+                            0.0 for 'home' or straight
         """
+
+        self.pwm = pwm
+        self.config = config
+        self.debug = debug
 
         for i in config.get('flip_direction', []):
             # Flip direction of motor
+            self._log("Flipping direction for motor %d" % i)
             self.forward[i], self.backwards[i] = self.backwards[i], self.forward[i]
+        self.speed_max = config.get('speed_max', 4000.0)
 
-        self.acceleration = config.get('acceleration', 20.0)
-        self.max_speed = config.get('max_speed', 200.0)
+        self.rotation_max = config.get('rotation_max', 50.0)
+        self.rotation_offset += config.get('rotation_offset', 0.0)
 
-        self.pins = self.motor_speed_controller \
-            + self.forward \
-            + self.backwards
 
-        for pin in self.pins:
-            GPIO.setup(pin, GPIO.OUT)   # Set all pins' mode as output
+    def start(self):
+        self.pins = self.forward + self.backwards
+        self._log("Setting up pins for output: %r" % self.pins)
+        GPIO.setup(self.pins, GPIO.OUT)   # Set all pins' mode as output
+
+        self.setSpeed(0.0)
+        self.setDirection(0.0)
+
+
+    def _log(self, message):
+        if self.debug:
+            sys.stderr.write(message + "\n")
+
+
+    def stop(self):
+        self.setSpeed(0.0)
+        self.setDirection(0.0)
+        self._log("Releasing GPIO pins: %r" % self.pins)
+        GPIO.cleanup(self.pins)
 
 
     def setSpeed(self, speed=0.0):
@@ -74,42 +105,70 @@ class Car:
         # Speed needs to be within -1.0 and 1.0 range
         assert(-1.0 <= speed <= 1.0)
 
-        if not speed\
+        if not speed \
                 or (self.speed > 0.0 and speed < 0.0) \
                 or (self.speed < 0.0 and speed > 0.0):
             # Direction changed or stop command given
             # Halt motors and reset speed
+            self._log("Dir changed: %.2f -> %.2f." % (self.speed, speed))
             for speed_ctrl in self.motor_speed_controller:
-                self.pwm.write(speed_ctrl, 0, 0.0)
+                self.pwm.write(speed_ctrl, 0, 0)
+            self._log("Set pins %r to %r" % (self.forward + self.backwards, GPIO.LOW))
+            GPIO.output(self.forward + self.backwards, GPIO.LOW)
 
-            for motor in self.forward + self.backward:
-                GPIO.output(motor, GPIO.LOW)
-
-        if speed != 0.0:
+        if speed:
             # Movement required. Set motor speed (always positive value)
-            for i in self.motor_speed_controller:
-                self.pwm.write(i, 0, abs(self.speed) * self.max_speed)
+            self._log("Set speed to %.2f = %d" % (
+                speed, int(abs(speed) * self.speed_max)))
+            for speed_ctrl in self.motor_speed_controller:
+                self.pwm.write(speed_ctrl, 0, int(abs(speed) * self.speed_max))
 
         if speed > 0.0:
             # Positive speed; Move forward
-            for motor in self.forward:
-                GPIO.output(motor, GPIO.LOW)
+            self._log("Set pins %r to %r" % (self.forward, GPIO.HIGH))
+            GPIO.output(self.forward, GPIO.HIGH)
         elif speed < 0.0:
             # Negative speed; move backward
-            for motor in self.backward:
-                GPIO.output(motor, GPIO.HIGH)
+            self._log("Set pins %r to %r" % (self.backwards, GPIO.HIGH))
+            GPIO.output(self.backwards, GPIO.HIGH)
 
         self.speed = speed
+
+    def setDirection(self, rotation=0.0):
+        # Set rotation ranging from -1.0 to 1.0
+        # Straight: rotation = 0.0
+        # Right: 0.0 > rotation >= 1.0
+        # Left: -1.0 <= rotation < 0.0
+
+        # Rotation needs to be within -1.0 and 1.0 range
+        assert(-1.0 <= rotation <= 1.0)
+
+        self._log("Set rotation to %.2f = %d + %d" % (
+            rotation, int(rotation * self.rotation_max), int(self.rotation_offset)))
+        for direction_ctrl in self.steering_controller:
+            self.pwm.write(direction_ctrl, 0, int(rotation * self.rotation_max + self.rotation_offset))
+
+        self.rotation = rotation
+
 
 
 if __name__ == '__main__':
     GPIO.setmode(GPIO.BOARD) # Number GPIOs by its physical location
     pwm = PWM() # The servo controller.
+    pwm.frequency = 60
 
-    car = Car(pwm)
+    config = {}
+    if os.path.exists('config.json'):
+        with open('config.json', 'r') as config_json:
+            config = json.load(config_json)
 
-    for speed in [0.5, 1.0, 0.5, 0.0, -0.5, -1.0, -0.5, 0.0]:
-        car.setSpeed(speed)
-        time.sleep(5)
+    car = Car(pwm, config=config, debug=True)
+    car.start()
 
+    for state in [0.5, 1.0, 0.5, 0.0, -0.5, -1.0, -0.5, 0.0]:
+        car.setSpeed(state)
+        car.setDirection(state)
+        time.sleep(0.5)
+
+    car.stop()
 
