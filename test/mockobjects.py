0ed5937521383b323d014988b1cdc4c1bfe2ddd5
Sebastiaan Pasterkamp <26205277+SebastiaanPasterkamp@users.noreply.github.com>
2017-03-14 21:44:25 +0100

    Tests: Improved MockOjbects logging calls and get/set attributes

--- test/mockobjects.py
+++ test/mockobjects.py
@@ -5,20 +5,33 @@ class MockObject(object):
     def __init__(self):
         self.log = []
         self.mocks = {}
+        self.mock_called = 'call'
 
-    def __getattr__(self, name, *args, **kwargs):
-        self.log.add(['get', name, args, kwargs])
+    def __getattr__(self, name):
+        if name == 'log' or name.startswith('mock'):
+            return self.__dict__[name]
 
         if name in self.mocks:
-            if callable(self.mocks[name]):
-                return self.mocks[name](*args, **kwargs)
-            return self.mocks[name]
+            if callable(self.__dict__['mocks'][name]):
+                self.mock_called = name
+                return self.__dict__['mocks'][name]
+            self.log.append(['get', name])
+            return self.__dict__['mocks'][name]
+        self.log.append(['get', name])
 
     def __setattr__(self, name, value):
-        self.log.add(['set', name, value])
+        if name in ['log', 'mocks', 'mock_called']:
+            self.__dict__[name] = value
+        else:
+            self.__dict__['log'].append(['set', name, value])
+            self.__dict__['mocks'][name] = value
 
-        self.mocks[name] = value
+    def mock_call(self, *args, **kwargs):
+        self.log.append([self.mock_called, args, kwargs])
+        pass
 
+    def mock_reset_log(self):
+        self.log = []
 
 class MockGPIO(MockObject):
     def __init__(self):
@@ -29,9 +42,16 @@ class MockGPIO(MockObject):
             'IN': 'IN',
             'HIGH': 'HIGH',
             'LOW': 'LOW',
-            'BOARD': 'BOARD'
+            'BOARD': 'BOARD',
+            'setup': self.mock_call,
+            'output': self.mock_call,
+            'setmode': self.mock_call
             }
 
-
 class MockPWM(MockObject):
-    pass
+    def __init__(self):
+        super(MockPWM, self).__init__()
+
+        self.mocks = {
+            'write': self.mock_call
+            }
