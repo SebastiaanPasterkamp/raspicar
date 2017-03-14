6c1313cff22147614997294c062533939d85c55f
Sebastiaan Pasterkamp <26205277+SebastiaanPasterkamp@users.noreply.github.com>
2017-03-14 20:26:54 +0100

    Added camera pan/tilt

--- modules/vehicle.py
+++ modules/vehicle.py
@@ -2,17 +2,24 @@
 """Vehicle set to provide control and motion models for various types of
 vehicles"""
 
-import os
 import sys
 import json
+import time
 
-sys.path.append(os.path.join(
-    os.path.dirname(os.path.realpath(__file__)), '..'
-    ))
 
-import RPi.GPIO as GPIO
-from sunfounder.PCA9685 import PWM
-import time    # Import necessary modules
+try:
+    import RPi.GPIO as GPIO
+    from sunfounder.PCA9685 import PWM
+except:
+    import os
+    sys.path.append(os.path.abspath(os.path.join(
+        os.path.dirname(__file__), '..'
+        )))
+
+    print sys.path
+
+    from test.mockobjects import MockGPIO as GPIO, MockPWM as PWM
+
 
 class Car(object):
     """Car type - uses a bicycle style motion model
@@ -29,8 +36,8 @@ class Car(object):
 
         config options:
         - flip_direction :  list of motors who's direction is flipped
-        - speed_max:        maximum speed for motor PWM controllers
-        - rotation_max :    maximum PWM for steering servo; maximum steering arc
+        - speed_max:        max speed for motor PWM controllers
+        - rotation_max :    max PWM for steering servo; maximum steering arc
         - rotation_offset : change in default PWM for steering servo
                             0.0 for 'home' or straight
         """
@@ -45,41 +52,39 @@ class Car(object):
 
         # Motor PWM pins
         self.motor_speed_controller = motor.get('pwm_speed_ctrl', [
-            4,  # servo driver IC CH4
-            5   # servo driver IC CH5
+            4,          # servo driver IC CH4
+            5           # servo driver IC CH5
             ])
 
         # Motor GPIO pins to set HIGH when moving forward
         self.forward = motor.get('forward', [
-            11, # pin11 - motor 0
-            13  # pin13 - motor 1
+            11,         # pin11 - motor 0
+            13          # pin13 - motor 1
             ])
         # Motor GPIO pins to set HIGH when moving backwards
         self.backwards = motor.get('backwards', [
-            12, # pin12 - motor 0
-            15  # pin15 - motor 1
+            12,         # pin12 - motor 0
+            15          # pin15 - motor 1
             ])
         # Max value for PWM
-        self.speed_max = motor.get('max_speed', 4000.0) 
+        self.speed_max = motor.get('max_speed', 4000.0)
         self.speed = 0.0
 
-        
         # Steering PWM pin
         self.steering_controller = steering.get('pwm_direction_ctrl', [
-            0 # servo driver IC CH0
+            0           # servo driver IC CH0
             ])
         self.rotation_max = steering.get('max_rotation', 75.0)
         # Default servo position for straight forward
         self.rotation_default = steering.get('default', 450.0)
         self.rotation = 0.0
 
-
         # Pan/Tilt PWM pins
         self.pan_controller = camera.get('pwm_pan_ctrl', [
-            14 # servo driver IC CH14
+            14          # servo driver IC CH14
             ])
         self.tilt_controller = camera.get('pwm_tilt_ctrl', [
-            15 # servo driver IC CH15
+            15          # servo driver IC CH15
             ])
         self.pan_max = camera.get('max_pan', 250.0)
         self.tilt_max = camera.get('max_tilt', 250.0)
@@ -97,7 +102,8 @@ class Car(object):
         for i in motor.get('flip_direction', []):
             # Flip direction of motor
             self._log("Flipping direction for motor %d" % i)
-            self.forward[i], self.backwards[i] = self.backwards[i], self.forward[i]
+            self.forward[i], self.backwards[i] \
+                = self.backwards[i], self.forward[i]
         self.rotation_default += steering.get('bias', 0.0)
         self.pan_default += camera.get('pan_bias', 0.0)
         self.tilt_default += camera.get('tilt_bias', 0.0)
@@ -105,27 +111,24 @@ class Car(object):
         # List of all GPIO pins in use
         self.pins = self.forward + self.backwards
 
-
     def start(self):
         self._log("Setting up pins for output: %r" % self.pins)
-        self.GPIO.setup(self.pins, self.GPIO.OUT)   # Set all pins' mode as output
+        # Set all pins' mode as output
+        self.GPIO.setup(self.pins, self.GPIO.OUT)
 
         self.setSpeed(0.0)
         self.setPanTilt(self.pan_home, self.tilt_home)
         self.setDirection(0.0)
 
-
     def _log(self, message):
         if self.debug:
             sys.stderr.write(message + "\n")
-
 
     def stop(self):
         self.setSpeed(0.0)
         self.setDirection(0.0)
         self._log("Releasing GPIO pins: %r" % self.pins)
         self.GPIO.cleanup(self.pins)
-
 
     def setSpeed(self, speed=0.0):
         # Set movement speed ranging from -1.0 to 1.0
@@ -173,7 +176,6 @@ class Car(object):
 
         self.speed = speed
 
-
     def setDirection(self, rotation=0.0):
         # Set rotation ranging from -1.0 to 1.0
         # Straight: rotation = 0.0
@@ -195,7 +197,6 @@ class Car(object):
                 )
 
         self.rotation = rotation
-
 
     def setPanTilt(self, pan=0.0, tilt=0.0):
         # Set pan/tilt ranging from -1.0 to 1.0
@@ -232,12 +233,15 @@ class Car(object):
 
 
 if __name__ == '__main__':
-    GPIO.setmode(GPIO.BOARD) # Number GPIOs by its physical location
-    pwm = PWM() # The servo controller.
+    GPIO.setmode(GPIO.BOARD)    # Number GPIOs by its physical location
+    pwm = PWM()                 # The servo controller.
     pwm.frequency = 60
 
+    import os
+
     config = {}
-    config_file = os.path.join(os.path.dirname(os.path.realpath(__file__)), '..', 'config.json')
+    config_file = os.path.join(os.path.abspath(
+        os.path.dirname(__file__), '..', 'config.json'))
     if os.path.exists(config_file):
         with open(config_file, 'r') as config_json:
             config = json.load(config_json)
@@ -264,4 +268,3 @@ if __name__ == '__main__':
         time.sleep(0.25)
 
     car.stop()
-
