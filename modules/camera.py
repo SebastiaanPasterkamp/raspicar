e4fa353d8cfe8d0649841be1e933c8e483f84bfc
Sebastiaan Pasterkamp <26205277+SebastiaanPasterkamp@users.noreply.github.com>
2017-03-29 23:40:50 +0200

    Camera: switched to cv2

--- modules/camera.py
+++ modules/camera.py
@@ -1,45 +1,43 @@
 """Computer Vision class to organize visual odometry"""
 
-import cv
+import cv2
+import numpy as np
 
 class VisualOdometry(object):
     def __init__(self, start_image,
                  min_features=100, max_features=200, min_distance=32):
 
-        self.size = cv.GetSize(start_image)
-        self.previous = cv.CreateImage(self.size, 8, 1)
-        self.current = cv.CreateImage(self.size, 8, 1)
-        cv.CvtColor(start_image, self.previous, cv.CV_BGR2GRAY)
-        cv.CvtColor(start_image, self.current, cv.CV_BGR2GRAY)
+        self.previous = cv2.cvtColor(start_image, cv2.COLOR_BGR2GRAY)
+        self.current = cv2.cvtColor(start_image, cv2.COLOR_BGR2GRAY)
 
         self.min_features = min_features
         self.max_features = max_features
         self.min_distance = min_distance
 
-        self.quality = 0.01
+        self.quality = 0.3
         self.features = []
 
         # Make sure we have features
         self.findNewFeatures()
 
     def followFeatures(self, new_image):
-        cv.CvtColor(new_image, self.current, cv.CV_BGR2GRAY)
+        self.current = cv2.cvtColor(new_image, cv2.COLOR_BGR2GRAY)
 
-        next_features, next_features_status, error = cv.CalcOpticalFlowPyrLK(
+        # calculate optical flow
+        old_features = np.float32(self.features)
+        new_features, status, error = cv2.calcOpticalFlowPyrLK(
             self.previous,
             self.current,
-            None, None,
-            self.features,
-            (int(self.min_distance), int(self.min_distance)),
-            0,
-            (cv.CV_TERMCRIT_ITER|cv.CV_TERMCRIT_EPS, 20, 0.03),
-            0
+            old_features,
+            winSize=(int(self.min_distance), int(self.min_distance)),
+            maxLevel=2,
+            criteria=(cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 0.03)
             )
 
         self.features = [
-            next_features[i]
-            for i in range(len(next_features))
-            if next_features_status[i]
+            tuple(feature.ravel())
+            for i, feature in enumerate(new_features)
+            if status[i]
             ]
 
         if len(self.features) < self.min_features:
@@ -50,46 +48,39 @@ class VisualOdometry(object):
 
     def findNewFeatures(self):
         # Avoid sampling new features around current features
-        mask = cv.CreateImage(self.size, 8, 1 )
-        cv.Set(mask, (255,255,255))
+        mask = np.zeros_like(self.current)
+        mask[:] = 255
 
-        for feature in self.features:
-            cv.Circle(
-                mask,
-                (int(feature[0]), int(feature[1])),
-                int(self.min_distance),
-                (0,0,0),
-                -1, 8, 0
-                )
+        for f in self.features:
+            cv2.circle(mask, f, 5, 0, -1)
 
-        new_features = cv.GoodFeaturesToTrack(
+        new_features = cv2.goodFeaturesToTrack(
             image=self.current,
-            eigImage=None,
-            tempImage=None,
-            cornerCount=self.max_features - len(self.features),
+            maxCorners=self.max_features - len(self.features),
             qualityLevel=self.quality,
             minDistance=self.min_distance,
             mask=mask
             )
 
-        self.features.extend(new_features)
+        if new_features is not None:
+            for x, y in np.float32(new_features).reshape(-1, 2):
+                self.features.append((x, y))
 
 if __name__ == '__main__':
-    capture = cv.CaptureFromCAM(-1)
+    capture = cv2.VideoCapture(-1)
 
+    ret, img = capture.read()
     odometry = VisualOdometry(
-        cv.QueryFrame(capture),
+        img,
         100, 150
         )
 
-    cv.NamedWindow("capture", cv.CV_WINDOW_AUTOSIZE)
-
     while True:
-        img = cv.QueryFrame(capture)
+        ret, img = capture.read()
         odometry.followFeatures(img)
 
         for feature in odometry.features:
-            cv.Circle(
+            cv2.circle(
                 img,
                 (int(feature[0]), int(feature[1])),
                 2,
@@ -97,10 +88,10 @@ if __name__ == '__main__':
                 -1, 8, 0
                 )
 
-        cv.ShowImage('capture', img)
-        c = cv.WaitKey(1)
+        cv2.imshow('img', img)
+        c = cv2.waitKey(1)
         if (c % 256) == 27:
             # ESC pressed
             break
 
-    del(capture)
+    capture.release()
